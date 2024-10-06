e432e8ecb27dc6a1a93c3011f31a0e4e52fb1d63
Nathan Wailes <nathan.wailes@gmail.com>
2024-10-06 18:05:43 +0700

    Update message.ts to be able to handle different log levels

--- utils/message.ts
+++ utils/message.ts
@@ -1,3 +1,7 @@
+import util from 'util';
+
+const debugLog = util.debuglog('app'); // Run the app with NODE_DEBUG=app to enable logging debug statements to the console.
+
 /**
  * Logs a message with a timestamp.
  * 
@@ -7,7 +11,7 @@
  * message('Application submitted successfully');
  * // Output: [13.04.2023 15:30:45] Application submitted successfully
  */
-const message = (text: string | Error) => {
+const message = (text: string | Error, level: 'info' | 'debug' | 'warn' | 'error' = 'info') => {
     const date = new Date();
     const options: Intl.DateTimeFormatOptions = {
         day: 'numeric',
@@ -20,7 +24,13 @@ const message = (text: string | Error) => {
     };
     const formattedDate = date.toLocaleString('en-US', options);
 
-    console.log(`[${formattedDate}] ${text}`);
+    const logMessage = `[${formattedDate}] [${level.toUpperCase()}] ${text}`;
+
+    if (level === 'debug') {
+        debugLog(logMessage);
+    } else {
+        console.log(logMessage);
+    }
 };
 
 export default message;
